bdc59f078c70a92dbd204d620243d75d0ff6a7fe
jiminseon <20201020@dongduk.ac.kr>
2025-09-23 11:29:41 +0900

    project 초기 세팅

---
+++ data-collect-module/src/main/java/com/example/data_collect_module/DataCollectModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.data_collect_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCollectModuleApplication.class, args);
+	}
+
+}
---
+++ data-process-module/src/test/java/com/example/data_process_module/DataProcessModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data_process_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProcessModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
