73497a6fee01c2a41d7b865ae2b80769a7aade29
jiminseon <min7un3@gmail.com>
2025-10-09 16:19:18 +0900

    Remove: search-modeul 삭제 #7

--- search-module/src/main/java/com/example/search_module/management/service/SearchService.java
+++
@@ -1,8 +0,0 @@
-package com.example.search_module.management.service;
-
-import com.example.search_module.management.dto.Request.SearchReq;
-import com.example.search_module.management.dto.Response.AlertResponseDto;
-
-public interface SearchService {
-    AlertResponseDto createAlert(SearchReq.SearchAlertReq dto, Long userId);
-}
