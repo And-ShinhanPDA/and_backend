93e63b24a00fc86d69ed2eaba7805c2f29ca4c82
jiminseon <min7un3@gmail.com>
2025-10-16 14:02:25 +0900

    Fix: stockCode 추가 #76

--- alert-module/src/main/java/com/example/alert_module/history/dto/AlertHistoryDto.java
+++ alert-module/src/main/java/com/example/alert_module/history/dto/AlertHistoryDto.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 public record AlertHistoryDto(
         Long id,
         Long alertId,
+        String stockCode,
         Boolean isSent,
         String indicatorSnapshot,
         LocalDateTime createdAt
@@ -14,6 +15,7 @@ public record AlertHistoryDto(
         return new AlertHistoryDto(
                 entity.getId(),
                 entity.getAlert().getId(),
+                entity.getAlert().getStockCode(),
                 entity.getIsSent(),
                 entity.getIndicatorSnapshot(),
                 entity.getCreatedAt()
