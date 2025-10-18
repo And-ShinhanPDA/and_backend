be4817f0f6d09ff204c128fe55ff46a9857cdbf4
jiminseon <min7un3@gmail.com>
2025-10-18 16:43:24 +0900

    feat: 푸쉬알림 #86

--- alert-module/src/main/java/com/example/alert_module/notification/message/AlertNotifier.java
+++ alert-module/src/main/java/com/example/alert_module/notification/message/AlertNotifier.java
@@ -1,24 +1,24 @@
-//package com.example.alert_module.notification.message;
-//
-//
-//import com.example.alert_module.notification.dto.AlertEvent;
-//import com.example.alert_module.notification.service.PushService;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-//import org.springframework.amqp.rabbit.annotation.RabbitListener;
-//import org.springframework.stereotype.Component;
-//
-//@Slf4j
-//@Component
-//@RequiredArgsConstructor
-//@RabbitListener(queues = "alert.queue", autoStartup = "true")
-//public class AlertNotifier {
-//    private final PushService pushService;
-//
-//    @RabbitHandler
-//    public void handleAlert(AlertEvent event) {
-//        log.info("📥 MQ 이벤트 수신: {}", event);
-//        pushService.send(event);
-//    }
-//}
+package com.example.alert_module.notification.message;
+
+
+import com.example.alert_module.notification.dto.AlertEvent;
+import com.example.alert_module.notification.service.PushService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@RabbitListener(queues = "alert.queue", autoStartup = "true")
+public class AlertNotifier {
+    private final PushService pushService;
+
+    @RabbitHandler
+    public void handleAlert(AlertEvent event) {
+        log.info("📥 MQ 이벤트 수신: {}", event);
+        pushService.send(event);
+    }
+}
