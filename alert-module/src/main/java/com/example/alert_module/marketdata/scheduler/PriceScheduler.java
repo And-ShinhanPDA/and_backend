51f943c8413d511817a4dcad26f079041825c335
jiminseon <min7un3@gmail.com>
2025-10-22 10:26:22 +0900

    Feat: 스케줄러 켜기 #116

--- alert-module/src/main/java/com/example/alert_module/marketdata/scheduler/PriceScheduler.java
+++ alert-module/src/main/java/com/example/alert_module/marketdata/scheduler/PriceScheduler.java
@@ -27,8 +27,7 @@ public class PriceScheduler {
 
     // 🕘 매일 오전 9시 (시가 알림)
 //    @Scheduled(cron = "0 0 9 * * MON-FRI", zone = "Asia/Seoul")
-//    @Scheduled(cron = "0 */3 * * * MON-FRI", zone = "Asia/Seoul")
-    @Scheduled(cron = "0 0 10 * * MON-FRI", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 30 10 * * MON-FRI", zone = "Asia/Seoul")
     public void sendOpenPriceAlerts() {
         processPriceAlerts("openPrice");
     }
